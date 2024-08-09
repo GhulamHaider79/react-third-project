d8dd4d2f1af25ec3fc608d6b1b96bacf7a2fdd83
GhulamHaider79 <127304370+GhulamHaider79@users.noreply.github.com>
2024-08-09 22:20:18 +0500

    just

--- react-third-project/src/components/About.jsx
+++ react-third-project/src/components/About.jsx
@@ -22,7 +22,7 @@ function About() {
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl flex justify-center items-center gap-12'>
 
     <img src={data.avatar_url} alt="Git picture" width={300} />
-    <h3>Github followers: {data.followers}</h3>
+    <h3> My Github followers: {data.followers}</h3>
     </div>
   )
 }
